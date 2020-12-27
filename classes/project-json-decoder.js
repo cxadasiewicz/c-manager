59c2e8ca25ea4fbef95efbaeecb8fcc06aac07e0
cxadasiewicz <cadasiewicz@gmail.com>
2020-12-27 10:17:33 -0500

    _

--- classes/project-json-decoder.js
+++ classes/project-json-decoder.js
@@ -13,22 +13,24 @@ const ProjectDecoder = require("./project-decoder");
 module.exports = class ProjectJSONDecoder extends ProjectDecoder {
 
 	get _components() { return "components"; }
+
 	get _variables() { return "variables"; }
+	get _variableDefault() { return "default"; }
+
 	get _products() { return "products"; }
+	get _productLocalInstallFolder() { return "install"; }
+	get _productPublicName() { return "public"; }
+	get _productBuildInfo() { return "build"; }
+
 	get _libraries() { return "libraries"; }
-	get _productImports() { return "imports"; }
-
-	get _installFolder() { return "install"; }
-	get _publicName() { return "public"; }
-	get _build() { return "build"; }
-
-	get _variableHead() { return "[["; }
-	get _variableTail() { return "]]"; }
-	get _inheritedValue() { return "inherit"; }
-
-	get _archiveSpecSeparator() { return "|"; }
 	get _gitHubLibrary() { return "github"; }
 	get _localLibrary() { return "local"; }
+
+	get _productImports() { return "imports"; }
+
+	get _specSeparator() { return "|"; }
+	get _variableHead() { return "[["; }
+	get _variableTail() { return "]]"; }
 
 	constructor() {
 		super();
@@ -50,11 +52,12 @@ module.exports = class ProjectJSONDecoder extends ProjectDecoder {
 		for (let key of Object.keys(data)) {
 			const value = this.resolveVariables(data[key]);
 			key = this.resolveVariables(key);
-			if (value != this._inheritedValue) {
+			if (!value.startsWith(this._variableDefault + this._specSeparator)) {
 				this.variables[key] = value;
 			} else if (!(key in this.variables)) {
-				this.variables[key] = "";
+				this.variables[key] = value.replace(this._variableDefault + this._specSeparator, "");
 			}
+
 		}
 	}
 
@@ -67,9 +70,9 @@ module.exports = class ProjectJSONDecoder extends ProjectDecoder {
 			const product = new Product();
 			product.name = this.resolveVariables(productKey);
 			const productData = data[productKey];
-			product.localInstallFolder = this.resolveVariables(productData[this._installFolder]);
-			product.publicName = this.resolveVariables(productData[this._publicName]);
-			const buildData = productData[this._build];
+			product.localInstallFolder = this.resolveVariables(productData[this._productLocalInstallFolder]);
+			product.publicName = this.resolveVariables(productData[this._productPublicName]);
+			const buildData = productData[this._productBuildInfo];
 			if (buildData) {
 				const buildInfo = new BuildInfo();
 				buildInfo.makefuncName = this.resolveVariables(buildData);
@@ -83,20 +86,20 @@ module.exports = class ProjectJSONDecoder extends ProjectDecoder {
 	addLibrariesDataToProject(data, project) {
 		if (!data) { return }
 		for (const libraryKey of Object.keys(data)) {
-			const archiveData = this.resolveVariables(data[libraryKey]).split(this._archiveSpecSeparator);
+			const libraryData = this.resolveVariables(data[libraryKey]).split(this._specSeparator);
 			let library;
-			switch (archiveData[0]) {
+			switch (libraryData[0]) {
 			case this._gitHubLibrary:
 				library = new GitHubLibrary();
-				library.publisherUsername = archiveData[1];
-				library.publishedBundleName = archiveData[2];
-				library.version = archiveData[3];
-				library.publishedTagPrefix = (archiveData.length > 4 ? archiveData[4] : "");
-				library.oauthToken = (archiveData.length > 5 ? archiveData[5] : "");
+				library.publisherUsername = libraryData[1];
+				library.publishedBundleName = libraryData[2];
+				library.version = libraryData[3];
+				library.publishedTagPrefix = (libraryData.length > 4 ? libraryData[4] : "");
+				library.oauthToken = (libraryData.length > 5 ? libraryData[5] : "");
 				break;
 			case this._localLibrary:
 				library = new LocalLibrary();
-				library.publishedBundlePath = archiveData[1];
+				library.publishedBundlePath = libraryData[1];
 				break;
 			}
 			library.name = this.resolveVariables(libraryKey);
